09b3cc1c838f24146990afe6db9b2cedfbdf4d88
Daniaaliss <dania.candia@gmail.com>
2024-07-30 17:30:09 -0400

    combinacion con main y ajustes

--- vitrina-project/src/pages/home.js
+++ vitrina-project/src/pages/home.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importa Bootstrap aquí
-import Footer from '../components/footer/footer.jsx';
-/* import MainContent from '../components/main/mainContent.jsx';  */
-import Navbar from '../components/nav/navbar.jsx';
-import Blog from '../components/blog/blog.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MainContent from '../components/main-ecommerce/mainContent.jsx';
+ 
  
 // Componente principal de la aplicación
 const Home = () => {
   return (
-    
+   
     <div className="Home">
       <div style={{ minHeight: 'calc(100vh - 120px)' }}>
-        <Blog />
+        <MainContent />
       </div>
     </div>
   );
 };
-
+ 
 export default Home
