30d2ec33906c6d8d1f35c8681bbad514422bb5fd
SDVG <Stefaniaguzman2516@gmail.com>
2024-07-30 14:46:43 -0400

    Ruta Blog arreglos

--- vitrina-project/src/pages/home.js
+++ vitrina-project/src/pages/home.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from '../components/nav/navbar.jsx';
-import Footer from '../components/footer/footer.jsx';
-import MainContent from '../components/main/mainContent.jsx';
 import Blog from './blog.js';
 // Componente principal de la aplicación
 function App() {
