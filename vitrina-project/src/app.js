56791d7d051632f95ea93381eb05dc69bd38b5f8
Myounnie <valeriaarrcardenas@gmail.com>
2024-07-31 17:21:01 -0400

    se solucionan conflictos

--- vitrina-project/src/app.js
+++ vitrina-project/src/app.js
@@ -5,11 +5,8 @@ import Footer from './components/footer/footer.jsx';
 import Blog from '../src/pages/blog/blog.js';
 import Home from '../src/pages/home.js';
 import Ventas from '../src/pages/ventas.js';
-<<<<<<< Updated upstream
 import AboutUs from './pages/about-us/aboutUs.jsx';
-=======
 import Donaciones from '../src/pages/donaciones.js'
->>>>>>> Stashed changes
 
 // Componente principal de la aplicación
 function App() {
@@ -21,11 +18,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/Ventas" element={<Ventas />} />
-<<<<<<< Updated upstream
         <Route path="/quienes-somos" element={<AboutUs />} />
-=======
         <Route path="/Donaciones" element={<Donaciones />} />
->>>>>>> Stashed changes
       </Routes>
       <Footer />
     </div>
