7b1c120b7c3a16bab9915e0bd346345f39d8e07c
Myounnie <valeriaarrcardenas@gmail.com>
2024-08-15 15:20:58 -0400

    Se agrega componente nosotros, se arreglan problemas de responsividad

--- vitrina-project/src/components/nosotros/nosotros.jsx
+++ vitrina-project/src/components/nosotros/nosotros.jsx
@@ -5,91 +5,91 @@ import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Nosotros = ()=>{
     return(
-        <section class="team section">
-    <div class="container">
-        <h1 class="section-title">Conoce a nuestro equipo</h1>
-        <div class="row justify-content-center">
-            <div class="col-md-4 col-sm-6 col-xs-12">
-                <div class="team-item">
+        <section className="team section">
+    <div className="container">
+        <h1 className="section-title">Conoce a nuestro equipo</h1>
+        <div className="row justify-content-center">
+            <div className="col-md-4 col-sm-6 col-xs-12 p-2 ">
+                <div className="team-item">
                     <figure>
-                        <img src="https://media.licdn.com/dms/image/D4D03AQELXiM9eUkuew/profile-displayphoto-shrink_800_800/0/1720714610670?e=1728518400&v=beta&t=BFz9yAA_Q5YiU6E8xP3hUxrvJsKw09gPbsxTdc5q77A" alt="Dania"/>
+                        <img src="https://lh3.googleusercontent.com/pw/AP1GczMUHKUkeRyuTTAn2jVvybVAMsxP1ZmEA6Q2vOzgSngi7skNiiWQCwGQ-IcBtzYhsajdKAln-8ldpYlHneiEZYIuXQoWSkBqGhROeLZm31ISVJ5UxlTlaDesHBUt9OeGc7F5LxoHYfrR2ntMYeVZXp8xCw=w800-h800-s-no-gm?authuser=0" alt="Dania"/>
                         <figcaption>
-                            <div class="info">
+                            <div className="info">
                                 <h3>Dania Candia</h3>
                                 <p>Minion </p>
                             </div>
-                            <div class="social">
-                                <a href="https://www.linkedin.com/in/dania-alisson-candia-fuentes-a7b0a4161/" class="LinkedIn" data-abc="true"><FontAwesomeIcon icon={faLinkedin} /></a>
-                                <a href="https://github.com/Daniaaliss" class="Github" data-abc="true"><FontAwesomeIcon icon={faGithub} /></a>
+                            <div className="social">
+                                <a href="https://www.linkedin.com/in/dania-alisson-candia-fuentes-a7b0a4161/" className="LinkedIn" data-abc="true"><FontAwesomeIcon icon={faLinkedin} /></a>
+                                <a href="https://github.com/Daniaaliss" className="Github" data-abc="true"><FontAwesomeIcon icon={faGithub} /></a>
                                 
                             </div>
                         </figcaption>
                     </figure>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-6 col-xs-12">
-                <div class="team-item">
+            <div className="col-md-4 col-sm-6 col-xs-12 p-2 "  >
+                <div className="team-item">
                     <figure>
-                        <img src="https://media.licdn.com/dms/image/D4E35AQGbsweYZooJmg/profile-framedphoto-shrink_800_800/0/1721311922019?e=1723762800&v=beta&t=3TuPa7c9szT0x1feEMKXTpLE2m-ymmENFSHGqqMYXzA" alt="Joan"/>
+                        <img src="https://lh3.googleusercontent.com/pw/AP1GczMN4s-JU4FlhV4ZiMe3tfqJ4qPYZx3MOcI2W5OUTggS-hgRwzp0x8Ti-nalQ9H9pPsqI4iz9cIl4eggVYij0b5n2T_Bv9QJrEUVDyBwLYDk0yfyAu7YxUEDJrX4Cnl5NWC39IEofJ13EGRrSL4hNLB9Lg=w1280-h1155-s-no-gm?authuser=0" alt="Joan"/>
                         <figcaption>
-                            <div class="info">
+                            <div className="info">
                                 <h3>Joan Rioseco</h3>
                                 <p>Minion</p>
                             </div>
-                            <div class="social">
-                                <a href="https://www.linkedin.com/in/joan-rioseco-barrera-9b0681305/" class="LinkedIn" data-abc="true"><FontAwesomeIcon icon={faLinkedin} /></a>
-                                <a href="https://github.com/Grimmzx" class="Github" data-abc="true"><FontAwesomeIcon icon={faGithub} /></a>
+                            <div className="social">
+                                <a href="https://www.linkedin.com/in/joan-rioseco-barrera-9b0681305/" className="LinkedIn" data-abc="true"><FontAwesomeIcon icon={faLinkedin} /></a>
+                                <a href="https://github.com/Grimmzx" className="Github" data-abc="true"><FontAwesomeIcon icon={faGithub} /></a>
                             </div>
                         </figcaption>
                     </figure>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-6 col-xs-12">
-                <div class="team-item">
+            <div className="col-md-4 col-sm-6 col-xs-12 p-2 ">
+                <div className="team-item">
                     <figure>
                         <img src="https://media.licdn.com/dms/image/v2/D4E03AQG1dLd4Zy5-8g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1701999326436?e=1728518400&v=beta&t=PCKnZMC2yunfU8Q53Z2cuM0W0Ns_Mi4ax06Tb7CbyPU" alt="Stefania"/>
                         <figcaption>
-                            <div class="info">
+                            <div className="info">
                                 <h3>Stefania</h3>
                                 <p>Minion</p>
                             </div>
-                            <div class="social">
-                                <a href="https://www.linkedin.com/in/stefania-guzman/" class="LinkedIn" data-abc="true"><FontAwesomeIcon icon={faLinkedin} /></a>
-                                <a href="https://github.com/SDVGB" class="Github" data-abc="true"><FontAwesomeIcon icon={faGithub} /></a>
+                            <div className="social">
+                                <a href="https://www.linkedin.com/in/stefania-guzman/" className="LinkedIn" data-abc="true"><FontAwesomeIcon icon={faLinkedin} /></a>
+                                <a href="https://github.com/SDVGB" className="Github" data-abc="true"><FontAwesomeIcon icon={faGithub} /></a>
                             </div>
                         </figcaption>
                     </figure>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-6 col-xs-12">
-                <div class="team-item">
+            <div className="col-md-4 col-sm-6 col-xs-12 p-2 ">
+                <div className="team-item">
                     <figure>
                         <img src="https://media.licdn.com/dms/image/v2/D4E03AQF2M2lDjHyWLQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1693797197679?e=1728518400&v=beta&t=e2Fn3qGkqvn_ytDxJ68jonNzD91ui_9o6CgeJIewVpI" alt="Lolo"/>
                         <figcaption>
-                            <div class="info">
+                            <div className="info">
                                 <h3>Loreto Godoy</h3>
                                 <p>Minion</p>
                             </div>
-                            <div class="social">
-                                <a href="https://www.linkedin.com/in/loreto-godoy-carrasco-2a605428a/" class="LinkedIn" data-abc="true"><FontAwesomeIcon icon={faLinkedin} /></a>
-                                <a href="https://github.com/LoloTamales" class="Github" data-abc="true"><FontAwesomeIcon icon={faGithub} /></a>
+                            <div className="social">
+                                <a href="https://www.linkedin.com/in/loreto-godoy-carrasco-2a605428a/" className="LinkedIn" data-abc="true"><FontAwesomeIcon icon={faLinkedin} /></a>
+                                <a href="https://github.com/LoloTamales" className="Github" data-abc="true"><FontAwesomeIcon icon={faGithub} /></a>
                             </div>
                         </figcaption>
                     </figure>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-6 col-xs-12">
-                <div class="team-item">
+            <div className="col-md-4 col-sm-6 col-xs-12 p-2 ">
+                <div className="team-item">
                     <figure>
                         <img src="https://media.licdn.com/dms/image/v2/D4E03AQFEX75y0hVpNg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1720709357021?e=1728518400&v=beta&t=pqEm1kwVmkQGmIQt0yXrII8erBhI5pvqc-nW7TllcoQ" alt="Vale"/>
                         <figcaption>
-                            <div class="info">
+                            <div className="info">
                                 <h3>Valeria Arredondo</h3>
                                 <p>Minion</p>
                             </div>
-                            <div class="social">
-                                <a href="https://www.linkedin.com/in/valeria-arredondo-478744299/" class="LinkedIn" data-abc="true"><FontAwesomeIcon icon={faLinkedin} /></a>
-                                <a href="https://github.com/Myounnie" class="Github" data-abc="true"><FontAwesomeIcon icon={faGithub} /></a>
+                            <div className="social">
+                                <a href="https://www.linkedin.com/in/valeria-arredondo-478744299/" className="LinkedIn" data-abc="true"><FontAwesomeIcon icon={faLinkedin} /></a>
+                                <a href="https://github.com/Myounnie" className="Github" data-abc="true"><FontAwesomeIcon icon={faGithub} /></a>
                             </div>
                         </figcaption>
                     </figure>
--- vitrina-project/src/pages/about-us/aboutUs.jsx
+++ vitrina-project/src/pages/about-us/aboutUs.jsx
@@ -6,16 +6,16 @@ const AboutUs = () => {
     return (
         <Fragment>
             <div id="cajaPrincipal">
-                <section className="layout">
-                    <div className="sidebar">
+                <section className="layout row">
+                    <div className="sidebar col-md-7">
                         <div>
                         <h1>Quienes somos</h1>
                         <h2>Vitrina, Un lugar para ser transparentes</h2>
                         <p className="parrafito">Vitrina, es un proyecto creado por el grupo homónimo del programa javascript de Generation, centrados en generar un espacio de apoyo a la comunidad mediante una plataforma que incite a la reutilizacion de prendas de vestir, para combatir la contaminacion causada por el exceso de ropa en basurales.</p>
 
                         </div>
-                        <div className="cajas">
-                            <div className="Minicaja">
+                        <div className="cajas row">
+                            <div className="Minicaja col-md-6">
                                 <div>
                                     <img src="https://img.icons8.com/?size=100&id=79030&format=png&color=1A1A1A" alt="iconoDinero" />
                                 </div>
@@ -24,7 +24,7 @@ const AboutUs = () => {
                                     <p>Para ser parte de esta comunidad no es necesario pagar. Nuestro pago es el ayudar a la ropa a tener una nueva vida</p>
                                 </div>
                             </div>
-                            <div className="Minicaja">
+                            <div className="Minicaja col-md-6">
                                 <div>
                                     <img src="https://img.icons8.com/?size=100&id=11253&format=png&color=1A1A1A" alt="iconoActualizar" />
                                 </div>
@@ -38,7 +38,7 @@ const AboutUs = () => {
                         
 
                     </div>
-                    <div className="body">
+                    <div className="body col-md-5">
                         <div className="bolsareciclable"> <img src="https://media.istockphoto.com/id/1307052468/es/foto/armario-de-segunda-mano-moda-circular-alquiler-intercambiar-idea-de-ropa.jpg?s=612x612&w=0&k=20&c=Cpl54deQw5RWd2MYwe1aQ63cnka35sm_Is2Ui6Huw4o=" alt="bolsaReutilizable" /></div>
                         <div className="fondosusc">
                             <p>Suscribete para más noticias</p>
