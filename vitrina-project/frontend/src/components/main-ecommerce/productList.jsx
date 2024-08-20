a100aff2067fff75be46f16ce723b9ff0f47c96d
Daniaaliss <dania.candia@gmail.com>
2024-08-20 11:21:43 -0400

    merge con la main 20-08

--- vitrina-project/frontend/src/components/main-ecommerce/productList.jsx
+++ vitrina-project/frontend/src/components/main-ecommerce/productList.jsx
@@ -4,9 +4,9 @@ import img01 from '../../assets/img/img01.jpeg';
 import img02 from '../../assets/img/img02.jpeg';
 import img03 from '../../assets/img/img03.jpeg';
 import img04 from '../../assets/img/img04.jpeg';
-import img05 from '../../assets/img/img05.jpeg';
-import img06 from '../../assets/img/img06.jpeg';
-import img07 from '../../assets/img/img07.jpeg';
+import img05 from '../../assets/img/img05.jpg';
+import img06 from '../../assets/img/img06.jpg';
+import img07 from '../../assets/img/img07.jpg';
 import img08 from '../../assets/img/img08.jpg';
 import img09 from '../../assets/img/img09.jpg';
 import img10 from '../../assets/img/img10.jpg';
@@ -20,7 +20,7 @@ import img17 from '../../assets/img/img17.jpeg';
 import img18 from '../../assets/img/img18.jpeg';
 import img19 from '../../assets/img/img19.jpeg';
 import img20 from '../../assets/img/img20.jpeg';
-import img21 from '../../assets/img/img21.jpeg';
+import img21 from '../../assets/img/img21.jpg';
 import img22 from '../../assets/img/img22.png';
 import ModalDetail from './modal/modalDetail';
 import './productList.css';
@@ -38,28 +38,28 @@ const ProductList = () => {
   };
 
   const products = [
-    { id: 1, name: 'Blouse and belt-skirt', img: img01 },
-    { id: 2, name: 'Pink ankara mixed gown', img: img02 },
-    { id: 3, name: 'A-shaped gown', img: img03 },
-    { id: 4, name: 'Blouse and belt-skirt', img: img04 },
-    { id: 5, name: 'Ankara suit', img: img05 },
-    { id: 6, name: 'Brown ball gown', img: img06 },
-    { id: 7, name: 'Male Suit', img: img07 },
-    { id: 8, name: 'Flared gown', img: img08 },
-    { id: 9, name: 'Blouse and belt-skirt', img: img09 },
-    { id: 10, name: 'Pink ankara mixed gown', img: img10 },
-    { id: 11, name: 'A-shaped gown', img: img11 },
-    { id: 12, name: 'Blouse and belt-skirt', img: img12 },
-    { id: 13, name: 'Ankara suit', img: img13 },
-    { id: 14, name: 'Brown ball gown', img: img14 },
-    { id: 15, name: 'Male Suit', img: img15 },
-    { id: 16, name: 'Flared gown', img: img16 },
-    { id: 17, name: 'Extra Product 1', img: img17 },
-    { id: 18, name: 'Extra Product 2', img: img18 }, 
-    { id: 19, name: 'Extra Product 3', img: img19 }, 
-    { id: 21, name: 'Extra Product 5', img: img20 }, 
-    { id: 22, name: 'Extra Product 6', img: img21 },
-    { id: 23, name: 'Extra Product 7', img: img22 } 
+    { id: 1, name: 'Ropa de niño', img: img01 },
+    { id: 2, name: 'Conjunto niño verano', img: img02 },
+    { id: 3, name: 'Poleron deportivo', img: img03 },
+    { id: 4, name: 'Pantalon de pijama', img: img04 },
+    { id: 5, name: 'Camisa bicolor', img: img05 },
+    { id: 6, name: 'Vestido vintage', img: img06 },
+    { id: 7, name: 'Blusa con pañuelo', img: img07 },
+    { id: 8, name: 'Conjunto vintage', img: img08 },
+    { id: 9, name: 'Cosmetiquero', img: img09 },
+    { id: 10, name: 'Ropa de niño', img: img10 },
+    { id: 11, name: 'Ropa de niña', img: img11 },
+    { id: 12, name: 'BConjunto vintage de bebe', img: img12 },
+    { id: 13, name: 'Pantalon de niña', img: img13 },
+    { id: 14, name: 'Ropa niña invierno', img: img14 },
+    { id: 15, name: 'Poleras niña', img: img15 },
+    { id: 16, name: 'Conjunto niño', img: img16 },
+    { id: 17, name: 'Ropa niño y niña', img: img17 },
+    { id: 18, name: 'Bolsos', img: img18 }, 
+    { id: 19, name: 'Carteras', img: img19 }, 
+    { id: 21, name: 'Conjunto pijama', img: img20 }, 
+    { id: 22, name: 'Blusa negra', img: img21 },
+    { id: 23, name: 'Cortavientos vintage', img: img22 } 
   ];
 
   const indexOfLastProduct = currentPage * productsPerPage;
