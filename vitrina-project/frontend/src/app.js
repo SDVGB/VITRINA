b054847ad9292503f3900214f24948a3ef5c353f
Joan Rioseco <yoanriba@gmail.com>
2024-08-22 11:45:19 -0400

    Separacion de logica carrito  de app , se crea useCart

--- vitrina-project/frontend/src/app.js
+++ vitrina-project/frontend/src/app.js
@@ -12,8 +12,8 @@ import LoginModal from '../src/components/login/loginmodal.jsx';
 import Carrito from '../src/components/Carrito/Carrito.jsx';
 import ProtectedRoute from '../src/components/nav/protectedRoute.jsx';  // Importa el componente de ruta protegida
 import Dashboard from './components/protected-Routes/Dashboard.jsx';  // Importa el nuevo componente seguro
+import { useCart } from './components/Carrito/useCart.js';  
 
-// Componente principal de la aplicación
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(() => {
     return localStorage.getItem('isAuthenticated') === 'true';
@@ -23,10 +23,9 @@ function App() {
     localStorage.setItem('isAuthenticated', isAuthenticated);
   }, [isAuthenticated]);
 
-
   const [showModal, setShowModal] = useState(false);
-  
-  // Hook para manejar el carro
+
+  // Hook para traer carrito
   const {
     cart,
     showConfirmation,
@@ -34,7 +33,7 @@ function App() {
     handleIncrement,
     handleDecrement,
     totalItemsInCart
-  } = useCart(); 
+  } = useCart();
 
   const handleLoginClick = () => {
     setShowModal(true);
@@ -46,15 +45,12 @@ function App() {
 
   return (
     <div id="root">
-      
-
-      {/* Mostrar mensaje de confirmación */}
+      {/* Alerta de confirmacion */}
       {showConfirmation && (
         <div className="alert alert-success text-center" role="alert">
           Producto agregado al carrito con éxito
         </div>
       )}
-      
 
       <div className="main-content">
         {isAuthenticated && <Logeado setIsAuthenticated={setIsAuthenticated} />}
@@ -63,14 +59,13 @@ function App() {
         <LoginModal show={showModal} handleClose={handleCloseModal} setIsAuthenticated={setIsAuthenticated} />
 
         <Routes>
-          
           <Route path="/" element={<Home isAuthenticated={isAuthenticated} />} />
           <Route path="/Blog" element={<Blog isAuthenticated={isAuthenticated} />} />
           <Route path="/Ventas" element={<Ventas isAuthenticated={isAuthenticated} handleAddToCart={handleAddToCart} />} />
           <Route path="/Quienes-somos" element={<AboutUs isAuthenticated={isAuthenticated} />} />
           <Route path="/Donaciones" element={<Donaciones isAuthenticated={isAuthenticated} />} />
           <Route path="/*" element={<Home isAuthenticated={isAuthenticated} />} />
-          
+
           <Route path="/carrito" element={
             <Carrito
               cart={cart}
@@ -79,11 +74,10 @@ function App() {
             />
           } />
           {/* Ruta Protegida */}
-          <Route path="/dashboard" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Dashboard /></ProtectedRoute>} 
-        />
+          <Route path="/dashboard" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Dashboard /></ProtectedRoute>} />
         </Routes>
       </div>
-      
+
       <Footer />
     </div>
   );
