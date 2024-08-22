b69d29386035f069ef972337561d9c5c1237773a
Stefania del Valle Guzman Brito <Stefaniaguzman2516@gmail.com>
2024-08-22 12:57:54 -0400

    mejoras al diseño del nav logeado

--- vitrina-project/frontend/src/components/nav/navbar.jsx
+++ vitrina-project/frontend/src/components/nav/navbar.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import './navbar.css'; 
+import nuevapublicacion from '../../assets/icons/nuevapublicacion.png'
+import profile from '../../assets/icons/profile.jpg'
+import './navbar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
-const Navbar = ({ onLoginClick, cartItemCount,isAuthenticated  }) => {
+
+const Navbar = ({ onLoginClick, cartItemCount, isAuthenticated }) => {
 
   const [menuOpen, setMenuOpen] = useState(false);
+
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -26,10 +32,31 @@ const Navbar = ({ onLoginClick, cartItemCount,isAuthenticated  }) => {
             <li><Link to="/Ventas" onClick={toggleMenu}>Ventas</Link></li>
             <li><Link to="/Donaciones" onClick={toggleMenu}>Donaciones</Link></li>
             <li><Link to="/Quienes-somos" onClick={toggleMenu}>Quiénes Somos</Link></li>
-            
+
             {/* Enlace al Dashboard solo si el usuario está autenticado */}
             {isAuthenticated && (
-              <li><Link to="/dashboard" onClick={toggleMenu}>Dashboard</Link></li>
+              <>
+                <li><Link to="/dashboard" onClick={toggleMenu}>Dashboard</Link></li>
+                <button className='BlogoUsuario' ><Link to="/Usuario" onClick={toggleMenu}><img src={profile} alt="logo-usuario" className='logoUsuario' /></Link></button>
+                {/* Botón que abre el submenú bajo 'Publicaciones' */}
+                <div className="btn-group" role="group">
+                  <button
+                    type="button"
+                    className="btn btn-primary dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    <img src={nuevapublicacion} alt="logo-publicaciones" className='logoPublicaciones' />
+                  </button>
+                  <ul className="dropdown-menu">
+                    <li><Link className="dropdown-item" to="/Publicaciones">Subir video</Link></li>
+                    <li><Link className="dropdown-item" to="/Publicaciones">Transmitir en vivo</Link></li>
+                    <li><Link className="dropdown-item" to="/Publicaciones">Crear publicación</Link></li>
+                  </ul>
+                </div>
+
+              </>
+
+
             )}
           </ul>
           <div className="navbar-login">
@@ -50,7 +77,7 @@ const Navbar = ({ onLoginClick, cartItemCount,isAuthenticated  }) => {
       </nav>
       <Outlet />
     </div>
-  ); 
+  );
 }
 
 export default Navbar;
