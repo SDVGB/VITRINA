c7d4ef9849e503d88b77e8be59c7a78c2d566b2b
Joan Rioseco <yoanriba@gmail.com>
2024-08-28 18:48:48 -0400

    Se arregla notificaciones

--- vitrina-project/frontend/src/app.js
+++ vitrina-project/frontend/src/app.js
@@ -34,7 +34,6 @@ function App() {
     localStorage.setItem('isAuthenticated', isAuthenticated);
   }, [isAuthenticated]);
 
-
   useEffect(() => {
     if (profileImage) {
       localStorage.setItem('profileImage', profileImage);
@@ -45,11 +44,6 @@ function App() {
   const [usuarioActual, setUsuarioActual] = useState(() => {
     return localStorage.getItem('usuarioActual') || ''; 
   });
-  
-
-  useEffect(() => {
-    localStorage.setItem('isAuthenticated', isAuthenticated);
-  }, [isAuthenticated]);
 
   useEffect(() => {
     localStorage.setItem('userRole', userRole);
@@ -59,11 +53,10 @@ function App() {
     localStorage.setItem('usuarioActual', usuarioActual);
   }, [usuarioActual]);
 
-
   const handleLoginSuccess = (role, userId) => {
     setIsAuthenticated(true);
     setUserRole(role);
-    setUsuarioActual(userId); // Actualiza el estado de usuario actual
+    setUsuarioActual(userId);
   };
 
   const handleLoginClick = () => {
@@ -74,8 +67,6 @@ function App() {
     setShowModal(false);
   };
 
-  // Hook para manejar el carrito
-
   const {
     cart,
     showConfirmation,
@@ -84,8 +75,6 @@ function App() {
     handleDecrement,
     totalItemsInCart
   } = useCart();
-
-
 
   return (
     <div id="root">
@@ -96,61 +85,46 @@ function App() {
       )}
 
       <div className="main-content">
-
-
         {isAuthenticated && <Logeado setIsAuthenticated={setIsAuthenticated} setProfileImage={setProfileImage} userRole={userRole}  />}
 
-        
         <Navbar 
           onLoginClick={handleLoginClick} 
           isAuthenticated={isAuthenticated} 
-          userRole={userRole}  // Asegúrate de pasar userRole aquí
+          userRole={userRole}
           cartItemCount={totalItemsInCart} 
           setIsAuthenticated={setIsAuthenticated} 
           profileImage={profileImage} 
-          setProfileImage={setProfileImage}  // Pass setProfileImage to Navbar
+          setProfileImage={setProfileImage}
         />
         
-
-
-     
-          
-
         <LoginModal 
-        show={showModal} 
-        handleClose={handleCloseModal} 
-        setIsAuthenticated={setIsAuthenticated}
-        setUserRole={setUserRole} 
-        setUsuarioActual={setUsuarioActual} // Pasar función para actualizar usuarioActual /
+          show={showModal} 
+          handleClose={handleCloseModal} 
+          setIsAuthenticated={setIsAuthenticated}
+          setUserRole={setUserRole} 
+          setUsuarioActual={setUsuarioActual}
         />
 
         <Routes>
           <Route path="/" element={<Home isAuthenticated={isAuthenticated} userRole={userRole} />} />
           <Route path="/Blog" element={<Blog isAuthenticated={isAuthenticated} userRole={userRole} />} />
-
           <Route path="/Ventas" element={<Ventas isAuthenticated={isAuthenticated} handleAddToCart={handleAddToCart} />} />
           <Route path="/Quienes-somos" element={<AboutUs isAuthenticated={isAuthenticated} />} />
           <Route path="/Donaciones" element={<Donaciones isAuthenticated={isAuthenticated} handleAddToCart={handleAddToCart} />} />
           <Route path="/notificaciones" element={<Notificaciones usuarioActual={usuarioActual} />} />
-
-
           <Route path="/perfil" element={<Perfil setProfileImage={setProfileImage} />} />
           <Route path="/*" element={<Home isAuthenticated={isAuthenticated} />} />
-
           <Route path="/carrito" element={<Carrito
               cart={cart}
               handleIncrement={handleIncrement}
               handleDecrement={handleDecrement}
             />
           } />
-          
           <Route path="/Publicaciones" element={
             <ProtectedRoute isAuthenticated={isAuthenticated}>
               <Publicaciones />
             </ProtectedRoute>
           } />
-          
-          
           <Route path="/Articulos" element={
             <ProtectedRoute isAuthenticated={isAuthenticated} userRole={userRole} requiredRole="Admin">
               <CrearArticulo />
